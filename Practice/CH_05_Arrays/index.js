37d39ea2ffa257ae3ccadf2da1c9033293c9ea89
0 |\/| <70907516+omkore@users.noreply.github.com>
2024-08-30 09:00:31 +0000

    Creation of Array

---
+++ Practice/CH_05_Arrays/index.js
@@ -0,0 +1,40 @@
+// Creating an Array using Array Literal
+
+let arr = [90,80,"jhon",true,undefined,null]
+// console.log(arr);
+// console.log(typeof arr);
+
+// // accessing array using index 
+// console.log(arr[3]);
+
+// modifying array 
+arr[3]="Third"
+console.log(arr);
+
+
+// // Lenght Property 
+// console.log(arr.length);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// Creating an Array using JavaScript new Keyword
+
+// let newArr1 = new Array("HTML","CSS","JavaScript")
+// console.log(newArr1);
+// console.log(typeof newArr1);
+// console.log(newArr1.length);
+
+// // Empty Array 
+// let newArr2 = new Array()
+// console.log(newArr2);
+// console.log(newArr2.length);
+
+// Empty Array with size 
+// let newArr3 = new Array(10)
+// // newArr3.push("html")
+// console.log(newArr3);
+// console.log(newArr3.length)
+
+
