dc20991082d6b0231d9a31665c74eb90d02e4365
0 |\/| <70907516+omkore@users.noreply.github.com>
2024-08-31 09:04:33 +0000

    array functions implemented

---
+++ Practice/CH_05_Arrays/ArrayMethods.js
@@ -0,0 +1,112 @@
+let arr = [10,20,"Hello",true]
+
+
+// length 
+// console.log(arr.length);
+
+//toString()
+// console.log(arr);
+
+// let a = arr.toString()
+// console.log(a);
+// console.log(typeof a);
+
+
+// join()
+// let j = arr.join("*")
+// console.log(j);
+// console.log(typeof j);
+
+
+
+// delete 
+
+// delete arr[0]
+
+// console.log(arr);
+
+
+// concat()
+
+// let arr2=[1,2,3,4,5]
+
+// let newArr = arr.concat(arr2)
+
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// push() 
+// console.log(arr);
+// arr.push(100)
+// console.log(arr);
+
+// unshift()
+
+// console.log(arr);
+// arr.unshift(0)
+// console.log(arr);
+
+
+// pop()
+
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+
+// shift()
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+
+// splice()
+
+let numArr = [20, 30, 40, 50];
+
+// let s = numArr.splice(1,3)
+// console.log(s);
+// console.log(numArr);
+
+
+// slice()
+
+
+//reverse()
+
+
+console.log(arr);
+console.log(arr.reverse());
+
+
+// values()
+// const fruits = ["Apple", "Banana", "Cherry"];
+
+// // Get the iterator object
+// const arrFruits = fruits.values();
+
+// // Iterate over the values using the iterator
+// for (const value of arrFruits) {
+//   console.log(value);
+// }
+
+
+// fill();
+
+
+let array = new Array(10);
+array.fill("hi")
+
+// console.log(array);
+
+
+
+//includes()
+let A = [1, 2, 3, 4, 5];
+
+// Include() function is called to
+// test whether the searching element
+// is present in given array or not.
+let a = A.includes(50);
+
+// Printing result of function.
+console.log(a);
