bda83946807517431a33c8f937099a4ea04be06d
0 |\/| <70907516+omkore@users.noreply.github.com>
2024-09-02 09:15:52 +0000

    array high order functoins implemented

---
+++ Practice/CH_05_Arrays/AdvancedArrayMEthods.js
@@ -0,0 +1,38 @@
+// Some() 
+
+// function divisibleBy2(element,index,array){
+//     return element%2==0
+// }
+
+// let arr = [11,31,5,1,31,5]
+
+// let ans = arr.some(divisibleBy2);
+// console.log(ans);
+
+//reduce()
+// let numbers = [88, 50, 25, 10];
+
+// // Performing reduce method
+// let sub = numbers.reduce(geeks);
+
+// function geeks(total, num,index,arr) {
+//     return total + num;
+// }
+
+// console.log(sub);
+
+
+
+// // Map() 
+
+// let numArr = [4, 9, 16, 25];
+
+// // Performing map method
+// function square(element,index,arr){
+//     return element*element
+// }
+
+// let ans = numArr.map(square)
+
+// console.log(typeof ans);
+// console.log(typeof numArr);
