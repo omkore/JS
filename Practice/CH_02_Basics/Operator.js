f04558e216dcc9f02ec098e9681551ebbca5291e
0 |\/| <70907516+omkore@users.noreply.github.com>
2024-09-03 10:38:35 +0000

    Operator Implemented

--- Practice/CH_02_Basics/Operator.js
+++ Practice/CH_02_Basics/Operator.js
@@ -1,17 +1,36 @@
 // Arithmetic 
-// +,-,*,/,% ,++,--
 
-let a=100,b=20;
+// let a =2;
+// let b= 3;
 
 // console.log(a+b);
 // console.log(a-b);
 // console.log(a*b);
 // console.log(a/b);
 // console.log(a%b);
+// console.log(2**2);
 
+// Unary 
+
+// let a = 10;
+// console.log(++a);//pre increment
+// console.log(++a);//pre decrement
 // console.log(--a);
-// console.log(a--);
-// console.log(--a);
+// console.log(a++); //post increment
+// console.log(a);
 
+// Relational / Comparison 
 
+let a = 10;
+let b= 100;
+
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a==b);//checks value only
+// console.log(a!=b);
+
+// console.log(10===10); // also check data type + value
+console.log(10==="10");
 
