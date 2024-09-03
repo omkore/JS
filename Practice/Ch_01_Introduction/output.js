320bb5cc8866b8a58b7885c53444141d29f007f9
0 |\/| <70907516+omkore@users.noreply.github.com>
2024-09-03 09:24:43 +0000

    variables

---
+++ Practice/Ch_01_Introduction/output.js
@@ -0,0 +1,16 @@
+alert("WElcome")
+
+let ans = confirm("Are you want exit...")
+document.write(ans);
+
+console.log("hello js");
+
+
+// document.getElementById("demo").innerText="<i>hi</i>"
+document.getElementById("demo").innerHTML="<i>hi</i>"
+
+
+let name = prompt("Enter your name")
+alert("Welcome "+name)
+document.write(name)
+
