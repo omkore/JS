2c7ec79a8a5ea1d99dd0d11d695f488a67173a60
0 |\/| <70907516+omkore@users.noreply.github.com>
2024-09-13 09:23:47 +0000

    Math object impleented

---
+++ Practice/CH_07_Maths/index.js
@@ -0,0 +1,46 @@
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.random()*10);
+
+
+
+
+// Math Methods 
+
+// abs()
+console.log(Math.abs(-10));
+
+// cbrt()
+console.log(Math.cbrt(27));
+
+// ceil()
+let a =4.1;
+console.log(Math.ceil(a));
+
+
+// floor()
+console.log(Math.floor(a));
+
+
+// fround()
+console.log(Math.round(a));
+
+
+
+
+
+
+
+// max() 
+console.log(Math.max(10,200,30));
+
+
+
+
+// min() 
+console.log(Math.min(10,200,30));
+
+
+// pow()
+
+console.log(Math.pow(10,3));
--- Practice/Ch_06_Strings/stringMethods.js
+++ Practice/Ch_06_Strings/stringMethods.js
@@ -18,3 +18,29 @@ console.log(string);
 
 
 
+
+// Define a string variable 
+let stone = "Soul";
+
+// Use the padStart() method to add padding characters "Mind "
+//to the beginning of the string 'stone' 
+// stone = stone.padStart(7, "Mind ");
+
+// Output the resulting string after padding
+// console.log(stone);
+
+
+// stone = stone.padEnd(8,"Mind")
+// console.log(stone);
+
+
+// str1 = "hello,this,is,string"
+
+
+// let arr = str1.split(",")
+
+// console.log(arr);
+
+
+var str="JavaScript is a scripting language. Scripting languages are often interpreted";
+console.log(str.search(/language/));
