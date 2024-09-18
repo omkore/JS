9370b3a71164927d5fc3a06ae78811aa3bd457b5
0 |\/| <70907516+omkore@users.noreply.github.com>
2024-09-18 09:35:28 +0000

    DOM Manipulation

--- Practice/CH_07_Maths/index.js
+++ Practice/CH_07_Maths/index.js
@@ -1,46 +1,52 @@
-console.log(Math.PI);
-console.log(Math.E);
-console.log(Math.random()*10);
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.random()*10);
 
 
 
 
-// Math Methods 
+// // Math Methods 
 
-// abs()
-console.log(Math.abs(-10));
+// // abs()
+// console.log(Math.abs(-10));
 
-// cbrt()
-console.log(Math.cbrt(27));
+// // cbrt()
+// console.log(Math.cbrt(27));
 
-// ceil()
-let a =4.1;
-console.log(Math.ceil(a));
-
-
-// floor()
-console.log(Math.floor(a));
+// // ceil()
+// let a =4.1;
+// console.log(Math.ceil(a));
 
 
-// fround()
-console.log(Math.round(a));
+// // floor()
+// console.log(Math.floor(a));
 
 
+// // fround()
+// console.log(Math.round(a));
 
 
 
 
 
-// max() 
-console.log(Math.max(10,200,30));
+
+
+// // max() 
+// console.log(Math.max(10,200,30));
 
 
 
 
-// min() 
-console.log(Math.min(10,200,30));
+// // min() 
+// console.log(Math.min(10,200,30));
 
 
-// pow()
+// // pow()
 
-console.log(Math.pow(10,3));
+// console.log(Math.pow(10,3));
+
+
+// log
+
+console.log(4&5)
+console.log(4|5)
---
+++ Practice/CH_08_Objects/index.js
@@ -0,0 +1,6 @@
+
+let heading = document.getElementById("heading")
+
+function changeText(){
+    heading.innerHTML="<i>Welcome To Disha Computers</i>"
+}
